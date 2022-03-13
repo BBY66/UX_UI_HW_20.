e06aa020ce967272be8c7d6c245423ff8a7da88e
BBY66 <89701424+BBY66@users.noreply.github.com>
2022-03-12 23:12:18 -0600

    .

---
+++ js/index.js
@@ -0,0 +1,14 @@
+console.log ("Your index.js file is loaded correctly!");
+$( "li" ).hover(
+    function() {
+      $( this ).append( $( "<span> ***</span>" ) );
+    }, function() {
+      $( this ).find( "span" ).last().remove();
+    }
+  );
+   
+  $( "li.fa" ).hover(function() {
+    $( this ).fadeOut( 100 );
+    $( this ).fadeIn( 500 );
+  })
+  
